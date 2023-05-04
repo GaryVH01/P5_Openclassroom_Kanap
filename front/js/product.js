3f666cc82600330d7decc9cad4bbeb0bea78b5a4
GaryVH <gary.vanhecke@gmail.com>
2023-05-04 11:52:32 +0200

    add condition (searchProduct != undefined)
    Page product done

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,8 @@
 const idProduct = new URLSearchParams(window.location.search).get("id")
-console.log(idProduct);
+// console.log(idProduct);
 
 const url = "http://localhost:3000/api/products/" + idProduct;
-console.log(url);
+// console.log(url);
 
 //On appelle la fonction fetch pour récupérer les informations (les ressources) sur le réseau.
 //On reçoit ensuite une promise (objet), que l'on va récupérer en format JSON. 
@@ -17,9 +17,11 @@ fetch(url)
     // la fonction catch permet d'afficher un message d'erreur si un problème survient.
     .catch(error => alert("Erreur : " + error));
 
+
+
 function displayProduct(response) {
 
-    console.log(response)
+    // console.log(response)
 
 
     //On récupère l'élément HTML auquel on veut ajouter l'image. Ensuite on crée la balise image.On définit la source de l'image.Puis on ajoute l'attribut alt à la balise img.
@@ -44,11 +46,11 @@ function displayProduct(response) {
     description.innerText = response.description;
 
     const colorTarget = document.getElementById("colors");
-    console.log(colorTarget)
+    // console.log(colorTarget)
 
 
     // On crée une boucle forEach pour ajouter toutes les couleurs en option de la balise select
-    // Dans le tableau colors, faire une boucle pour chaque élément du tableau
+    // Dans le tableau colors, faire une boucle pour chaque élément du tableau.
     response.colors.forEach((color) => {
         const select = document.getElementById("colors");
         const option = document.createElement("option");
@@ -56,7 +58,7 @@ function displayProduct(response) {
         // Récupération des données de l'API
         option.value = color;
         option.innerText = color;
-
+        // console.log(color);
         select.appendChild(option);
     })
 
@@ -67,34 +69,86 @@ function displayProduct(response) {
     descriptionTarget.appendChild(description);
 
 
+    // Création du panier
+    // récupération du boutton addToCart et ajout d'un écouteru d'événements
     const boutton = document.getElementById("addToCart");
     boutton.addEventListener("click", (e) => {
         e.preventDefault();
-        console.log("je clique");
+        // console.log("je clique");
 
-        let product = {
-            id: "3333",
-            quantity: "23"
+        // On stocke la quantité et la couleur choisie dans des variables pour les réutiliser ensuite.
+        const color = document.getElementById("colors");
+        let colorSelected = color.value;
+        const quantity = document.getElementById("quantity");
+        let quantitySelected = Number(quantity.value);
+
+        // Variable product, dans laquelle on stocke un objet comprenant (id, quantity et color)
+        //Si quantité supérieure à 0 et couleur sélectionnée ET nombre entier, on appelle la fonction addToCart.
+        if (colorSelected != "" && quantitySelected > 0 && Number.isInteger(quantitySelected)) {
+            const product = {
+                id: idProduct,
+                quantity: quantitySelected,
+                color: colorSelected
+            }
+
+            // console.log(product);
+            addToCart(product)
+
+        } else {
+            alert("Vous devez sélectionner une quantité valide (nombre entier) et une couleur")
         }
 
-        addToCart(product)
-
     })
+
 }
 
-function addToCart(product) {
-    let cart = [];
-    cart.push(product);
+/* ----------------------------------------Les FONCTIONS-------------------------------------------------*/
+
+// Création d'une fonction permettant de stocker le panier dans le localStorage
+function saveToLocalStorage(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-/** création du panier
- * - Ajout d'un eventListener sur le bouton panier au clic.
- * - Stockage des éléments, qui vont être ajoutés dans le panier, dans le localStorage en créant un tableau avec [id, qunatity, color]
- * - 
- * - 
- * 
- * Je dois ajouter un tableau d'objets 
- * 
- * 
- */
+// Création d'une fonction permettant de récupèrer le panier du localStorage
+function getCart() {
+    let cart = localStorage.getItem("cart");
+    if (cart === null) { //s'il n'y a rien dans le localStorage, on récupère un array.
+        return [];
+    } else {  //sinon on récupère un objet JSON du panier.
+        return JSON.parse(cart);
+    }
+}
+
+// Création d'une fonction d'ajout au panier
+function addToCart(product) {
+    let cart = getCart();
+    let searchProduct = cart.find(el => el.id === product.id && el.color === product.color); // fonction find() pour vérifier, dans le tableau, la présence d'un élément.
+    // console.log(searchProduct);
+    // ajout condition pour quantité
+if (searchProduct != undefined) {
+    searchProduct.quantity += product.quantity;
+} else {
+    cart.push(product);
+}
+    saveToLocalStorage(cart);
+}
+
+
+//-----------------Ancienne fonction à retravailler--------------------
+/*
+function addToCart(product) {
+        let cart = localStorage.getItem("cart")
+        if (!cart || cart.length === 0) {
+        cart.push(product);
+        window.localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+        const getRefreshCart = window.localStorage.getItem('cart', JSON.stringify(cart));
+        const refreshCart = JSON.parse(getRefreshCart);
+        refreshCart.push(cart);
+        window.localStorage.setItem('cart', JSON.stringify(refreshCart));
+        // console.log("ajout du nouveau panier")
+}
+}
+*/
+
+
